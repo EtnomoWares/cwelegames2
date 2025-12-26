ffdd328e902d51e9a5c7945a4b162f122f2212bf
warzywunio <mularczyk.piotr2006@gmail.com>
2025-12-26 16:42:35 +0100

    test2

--- Assets/scripts/scritchuj.cs
+++ Assets/scripts/scritchuj.cs
@@ -5,7 +5,7 @@ public class scritchuj : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int jubel = 0;
     }
 
     // Update is called once per frame
